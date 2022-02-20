5d220933e5d4781789b53ab85c85d3307227c36c
Mahmood Bawazir <mbzcodes@gmail.com>
2022-02-20 14:28:27 +0400

    remove full name field from signup form

--- src/components/forms/signupForm.tsx
+++ src/components/forms/signupForm.tsx
@@ -8,7 +8,6 @@ const SignupForm: React.FC<{}> = () => {
   return (
     <FormWrapper>
       <form>
-        <Input type="text" placeholder="Full Name" />
         <Input type="email" placeholder="Email address" />
         <Input type="password" placeholder="Your password" />
         <PrimaryButton size={'full'}>Sign up</PrimaryButton>
